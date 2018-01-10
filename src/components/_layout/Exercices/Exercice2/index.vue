04a09419735483188085d69fd6f68845e6aba8b3
olivorel <olivorel45@hotmail.fr>
2018-01-10 14:03:45 +0100

    affichage de chacun des composants ok

--- src/components/_layout/Exercices/Exercice2/index.vue
+++ src/components/_layout/Exercices/Exercice2/index.vue
@@ -30,23 +30,39 @@
     <div class="separator">
     </div>
     <div class="right">
-      Liste des composants : <span @click="changeTheDisplayedComponent('composant1')">composants1</span>, <span @click="changeTheDisplayedComponent('composant2')">composant2</span>, <span @click="changeTheDisplayedComponent('composant3')">composant3</span>
+      Liste des composants :<span @click="changeTheDisplayedComponent('composant1')">composants1</span><span
+            @click="changeTheDisplayedComponent('composant2')">composants2</span><span @click="changeTheDisplayedComponent('composant3')">composants3</span>
       <!-- ton code ici -->
+      <div class="Listcomponents">
+        <ul>
+          <li v-if="list"><component1></component1></li>
+          <!--<li v-if="list"><component2></component2></li>-->
+          <!--<li v-if="list"><component3></component3></li>-->
+        </ul>
+      </div>
     </div>
   </div>
   </div>
 </template>
 <script>
-export default {
-  data () {
-    return {
-    }
-  },
-  methods: {
+  import component1 from './_subs/Component1.vue'
+  import Component2 from './_subs/Component2.vue'
+  import Component3 from './_subs/Component3.vue'
 
-  },
-  components: {
-  }
+export default {
+    data () {
+      return {
+        list: false
+      }
+    },
+    methods: {
+      changeTheDisplayedComponent: function () {
+        this.list = true
+      }
+    },
+    components: {
+      component1, Component2, Component3
+    }
 }
 </script>
 <style>
