3d448e2e695ca2d50d368e043c5a21c36f92d41c
olivorel <olivorel45@hotmail.fr>
2018-01-11 22:00:27 +0100

    essais

---
+++ src/components/_layout/Exercices/Exercice3/_commons/mycomponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="donnee" v-if="donnee">
+        <p v-model="donnee.title">{{donnee.title}}</p>
+        <p v-model="donnee.content"> {{donnee.content}}</p>
+        <p v-model="donnee.question">{{donnee.question}}</p>
+    </div>
+
+</template>
+<script>
+  export default {
+    data () {
+      return {}
+    },
+    methods: {},
+    components: {}
+  }
+</script>
