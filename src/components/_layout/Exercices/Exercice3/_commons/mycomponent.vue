877da83bacb06a4d56d8d3051f3e0f580313dffd
olivorel <olivorel45@hotmail.fr>
2018-01-11 22:03:00 +0100

    erreur v-model

--- src/components/_layout/Exercices/Exercice3/_commons/mycomponent.vue
+++ src/components/_layout/Exercices/Exercice3/_commons/mycomponent.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="donnee" v-if="donnee">
-        <p v-model="donnee.title">{{donnee.title}}</p>
-        <p v-model="donnee.content"> {{donnee.content}}</p>
-        <p v-model="donnee.question">{{donnee.question}}</p>
+        <p>{{donnee.title}}</p>
+        <p> {{donnee.content}}</p>
+        <p>{{donnee.question}}</p>
     </div>
 
 </template>
